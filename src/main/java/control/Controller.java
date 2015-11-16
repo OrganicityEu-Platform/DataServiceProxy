9293cda70440ecc8a05144f38c7d0bac55b6ef88
Evangelos Theodoridis <evangelos.theodoridis@intel.com>
2015-11-16 17:44:49 +0000

    requests

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -2,11 +2,14 @@ package control;
 
 import domain.smartcitizen.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import service.LondonAPIService;
 import service.ModelService;
 import service.SantanderAPIService;
 import service.SmartphoneAPIService;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class Controller {
@@ -30,34 +33,44 @@ public class Controller {
                             @RequestParam(value = "function", required = false) String function,
                             @RequestParam(value = "rollup", required = false) String rollup, //m, h , d
                             @RequestParam(value = "limit", required = false) String limit,
-                            @RequestParam(value = "offset", required = false) String offset) throws Exception {
-        if (uuid.startsWith("urn:oc:entity:santander") == true) {
-            try {
-                uuid = uuid.replace(":", "_");
-                //attribute_id = attribute_id.replace(":", "_");
-                if (attribute_id.startsWith("urn:oc:attributeType:") ==true) {
-                    attribute_id = attribute_id.replace("urn:oc:attributeType:","");
+                            @RequestParam(value = "offset", required = false) String offset,
+                            HttpServletResponse response) throws Exception {
+        Response r = null;
+        try {
+            if (uuid.startsWith("urn:oc:entity:santander") == true) {
+                try {
+                    uuid = uuid.replace(":", "_");
+                    //attribute_id = attribute_id.replace(":", "_");
+                    if (attribute_id.startsWith("urn:oc:attributeType:") == true) {
+                        attribute_id = attribute_id.replace("urn:oc:attributeType:", "");
+                    }
+                    r = modelService.getSmartCitizenResponse(santanderAPIService.getData(uuid, "urn_oc_entityType_iotdevice", attribute_id, start, end, rollup, function, offset, limit), start, end, function, rollup);
+                } catch (Exception e) {
+                    throw e;
                 }
-                return modelService.getSmartCitizenResponse(santanderAPIService.getData(uuid, "urn_oc_entityType_iotdevice", attribute_id, start, end, rollup, function, offset, limit), start, end, function, rollup);
-            } catch (Exception e) {
-                throw e;
-            }
-        } else if (uuid.startsWith("urn:oc:entity:london:smartphone") == true) {
-            try {
-                return modelService.getSmartCitizenResponse2(smartphoneAPIService.getData(uuid, attribute_id, start, end, rollup, function, offset, limit), start, end, function, rollup);
-            } catch (Exception e) {
-                throw e;
-            }
-        } else if (uuid.startsWith("urn:oc:entity:london:") == true) {
-            try {
+            } else if (uuid.startsWith("urn:oc:entity:london:smartphone") == true) {
+                try {
+                    r = modelService.getSmartCitizenResponse2(smartphoneAPIService.getData(uuid, attribute_id, start, end, rollup, function, offset, limit), start, end, function, rollup);
+                } catch (Exception e) {
+                    throw e;
+                }
+            } else if (uuid.startsWith("urn:oc:entity:london:") == true) {
+                try {
 
-                return londonAPIService.getData(uuid, attribute_id, start, end, rollup, function, offset, limit);
-            } catch (Exception e) {
-                throw e;
+                    r = londonAPIService.getData(uuid, attribute_id, start, end, rollup, function, offset, limit);
+                } catch (Exception e) {
+                    throw e;
+                }
             }
+            if (r.getReadings() == null || r.getReadings().length == 0) {
+                response.sendError(HttpStatus.NOT_FOUND.value());
+                return null;
+            }
+        } catch (Exception e) {
+            response.sendError(HttpStatus.BAD_REQUEST.value());
+            return null;
         }
-
-        return null;
+        return r;
     }
 
 
